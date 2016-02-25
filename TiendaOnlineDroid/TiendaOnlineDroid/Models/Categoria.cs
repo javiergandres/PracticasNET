900e01e05d47131e6945629da037f9e07192781c
Ángel Salvatierra <angelsalvin@gmail.com>
2016-02-25 17:02:37 +0100

    Modelos añadidos a TiendaOnlineDroid

---
+++ TiendaOnlineDroid/TiendaOnlineDroid/Models/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiAdventure.Models
+{
+    public class Categoria
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+
+    }
+}
