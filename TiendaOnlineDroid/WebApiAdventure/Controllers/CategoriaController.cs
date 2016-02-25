50f47287a63f3e3bda9d6b52696a76690d067fa9
Sergio Hernández <hernandezbs@live.com>
2016-02-25 15:07:42 +0100

    Creación de controladores para Web Api, Producto, Categoría y Sub-Categoría

---
+++ TiendaOnlineDroid/WebApiAdventure/Controllers/CategoriaController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiAdventure.AdventureWork;
+
+namespace WebApiAdventure.Controllers
+{
+    public class CategoriaController : ApiController
+    {
+        private AdWorksEntities db = new AdWorksEntities();
+
+        // GET: api/Categoria
+        public IQueryable<ProductCategory> GetProductCategory()
+        {
+            return db.ProductCategory;
+        }
+
+        // GET: api/Categoria/5
+        [ResponseType(typeof(ProductCategory))]
+        public IHttpActionResult GetProductCategory(int id)
+        {
+            ProductCategory productCategory = db.ProductCategory.Find(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productCategory);
+        }
+
+        // PUT: api/Categoria/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProductCategory(int id, ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != productCategory.ProductCategoryID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(productCategory).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductCategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Categoria
+        [ResponseType(typeof(ProductCategory))]
+        public IHttpActionResult PostProductCategory(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ProductCategory.Add(productCategory);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = productCategory.ProductCategoryID }, productCategory);
+        }
+
+        // DELETE: api/Categoria/5
+        [ResponseType(typeof(ProductCategory))]
+        public IHttpActionResult DeleteProductCategory(int id)
+        {
+            ProductCategory productCategory = db.ProductCategory.Find(id);
+            if (productCategory == null)
+            {
+                return NotFound();
+            }
+
+            db.ProductCategory.Remove(productCategory);
+            db.SaveChanges();
+
+            return Ok(productCategory);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductCategoryExists(int id)
+        {
+            return db.ProductCategory.Count(e => e.ProductCategoryID == id) > 0;
+        }
+    }
+}
---
+++ TiendaOnlineDroid/WebApiAdventure/Controllers/SubCategoriaController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiAdventure.AdventureWork;
+
+namespace WebApiAdventure.Controllers
+{
+    public class SubCategoriaController : ApiController
+    {
+        private AdWorksEntities db = new AdWorksEntities();
+
+        // GET: api/SubCategoria
+        public IQueryable<ProductSubcategory> GetProductSubcategory()
+        {
+            return db.ProductSubcategory;
+        }
+
+        // GET: api/SubCategoria/5
+        [ResponseType(typeof(ProductSubcategory))]
+        public IHttpActionResult GetProductSubcategory(int id)
+        {
+            ProductSubcategory productSubcategory = db.ProductSubcategory.Find(id);
+            if (productSubcategory == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(productSubcategory);
+        }
+
+        // PUT: api/SubCategoria/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProductSubcategory(int id, ProductSubcategory productSubcategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != productSubcategory.ProductSubcategoryID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(productSubcategory).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductSubcategoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/SubCategoria
+        [ResponseType(typeof(ProductSubcategory))]
+        public IHttpActionResult PostProductSubcategory(ProductSubcategory productSubcategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ProductSubcategory.Add(productSubcategory);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = productSubcategory.ProductSubcategoryID }, productSubcategory);
+        }
+
+        // DELETE: api/SubCategoria/5
+        [ResponseType(typeof(ProductSubcategory))]
+        public IHttpActionResult DeleteProductSubcategory(int id)
+        {
+            ProductSubcategory productSubcategory = db.ProductSubcategory.Find(id);
+            if (productSubcategory == null)
+            {
+                return NotFound();
+            }
+
+            db.ProductSubcategory.Remove(productSubcategory);
+            db.SaveChanges();
+
+            return Ok(productSubcategory);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductSubcategoryExists(int id)
+        {
+            return db.ProductSubcategory.Count(e => e.ProductSubcategoryID == id) > 0;
+        }
+    }
+}
