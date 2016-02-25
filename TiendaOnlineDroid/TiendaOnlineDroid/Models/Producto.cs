cb51e987c1c2967f0b013fe3fc2fb76fb55f4281
Sergio Hernández <hernandezbs@live.com>
2016-02-25 17:21:38 +0100

    Coreccion en modelos Droid que ya estaban corregidos

--- TiendaOnlineDroid/TiendaOnlineDroid/Models/Producto.cs
+++ TiendaOnlineDroid/TiendaOnlineDroid/Models/Producto.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
-namespace WebApiAdventure.Models
+
+namespace TiendaOnlineDroid.Models
 {
     public class Producto
     {
