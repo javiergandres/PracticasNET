ba1c1ac76591a72c9a46794b9672c2125443c561
Ángel Salvatierra <angelsalvin@gmail.com>
2016-02-25 16:43:32 +0100

    Cambios Main

--- TiendaOnlineDroid/TiendaOnlineDroid/Resources/Resource.Designer.cs
+++ TiendaOnlineDroid/TiendaOnlineDroid/Resources/Resource.Designer.cs
@@ -60,8 +60,17 @@ namespace TiendaOnlineDroid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050001
+			public const int spinnerCategoria = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int spinnerSubcategoria = 2131034115;
+			
 			// aapt resource value: 0x7f050000
-			public const int MyButton = 2131034112;
+			public const int textView1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int textView2 = 2131034114;
 			
 			static Id()
 			{
